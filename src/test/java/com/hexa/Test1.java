f71701f17facd8dc66efb253828b94a0001a275f
sivaramakrishnan97 <sivaraman.drs@gmail.com>
2023-04-14 07:39:06 +0530

    Test 1

---
+++ src/test/java/com/hexa/Test1.java
@@ -0,0 +1,14 @@
+package com.hexa;
+
+public class Test1 {
+	private void Tamil() {
+		System.out.println("Tamil");
+	}
+	private void English() {
+		System.out.println("English");
+	}
+	private void Telugu() {
+		System.out.println("Telugu");
+	}
+
+}
